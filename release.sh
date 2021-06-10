a336350c69d8e3cd822c7a439f62e0fdc77dc722
A248 <theanandbeh@gmail.com>
2021-06-10 12:17:49 -0400

    Updates submodules and modify release.sh to also update submodules

--- release.sh
+++ release.sh
@@ -5,8 +5,8 @@ mvn versions:set -DnewVersion=$VERSION
 
 mvn clean deploy || exit 1
 
-git add pom.xml && git add SolarPaper-Assistant/pom.xml && git commit -m "Release $VERSION"
 cd SolarPaper-API && git add pom.xml && git commit -m "Release $VERSION" && cd ..
 cd SolarPaper-Server && git add pom.xml && git commit -m "Release $VERSION" && cd ..
+git add . && git commit -m "Release $VERSION"
 
 git push && cd SolarPaper-API && git push && cd ../SolarPaper-Server && git push && cd ..
