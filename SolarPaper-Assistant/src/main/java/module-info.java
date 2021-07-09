5e868de49b10fdbe4ddaa9bebc2ad8b42199de5b
A248 <theanandbeh@gmail.com>
2021-07-09 17:58:30 -0400

    Fix event executor generation

--- SolarPaper-Assistant/src/main/java/module-info.java
+++ SolarPaper-Assistant/src/main/java/module-info.java
@@ -4,6 +4,6 @@ module gg.solarmc.serverassistant {
 
     requires org.bukkit;
 
-    exports gg.solarmc.serverassistant to java.base;
+    exports gg.solarmc.serverassistant;
     provides com.destroystokyo.paper.event.executor.ClassDefinerFactory with ModularClassDefiner;
 }
