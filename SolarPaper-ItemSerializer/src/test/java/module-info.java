be702b3d9d6607a7f0c5f14b15512e49f383eb2f
A248 <theanandbeh@gmail.com>
2021-07-27 07:15:17 -0400

    Add SolarPaper-ItemSerializer (#2)

---
+++ SolarPaper-ItemSerializer/src/test/java/module-info.java
@@ -0,0 +1,7 @@
+open module gg.solarmc.paper.itemserializer.test {
+    requires gg.solarmc.paper.itemserializer;
+    requires org.junit.jupiter.api;
+    requires org.mockito.junit.jupiter;
+    requires org.mockito;
+    requires net.bytebuddy; // Needed by mockito
+}
